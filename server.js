66b8823f7d393068c6ff34cfa5e463e6c18d185f
Malvina Hasa <hasamalvina24@gmail.com>
2022-11-08 23:12:49 -0500

    Added server.js file and PORT to heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;

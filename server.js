2ead4b31bff03ebf297d1231440997d0ada1f5db
Malvina Hasa <hasamalvina24@gmail.com>
2022-11-10 22:50:15 -0500

    Added imports and some routes

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
-
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
